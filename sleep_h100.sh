0abc3b551d97bbceefb3645782b2e1a6be76cf72
Xichen Pan <xichenpan@meta.com>
2024-06-24 13:06:04 -0700

    add sleep

---
+++ sleep_h100.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --account=genai_interns
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --gres=gpu:1
+#SBATCH -t 14-00:00:00
+#SBATCH --output=/dev/null
+#SBATCH --error=/dev/null
+
+sleep infinity
