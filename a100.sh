a8a2cf0ee3ab0d47fa03090ece5e4a545d4c1d83
Xichen Pan <xichenpan@meta.com>
2024-06-24 00:17:41 -0700

    #SBATCH --ntasks-per-node=8

--- a100.sh
+++ a100.sh
@@ -4,8 +4,8 @@
 #SBATCH --qos=genai_interns
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=8
-#SBATCH --cpus-per-task=64
-#SBATCH --gres=gpu:12
+#SBATCH --cpus-per-task=12
+#SBATCH --gres=gpu:8
 #SBATCH -t 14-00:00:00
 #SBATCH --output=out
 #SBATCH --error=err
